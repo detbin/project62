0bd36290d84b4271c9f6b29d51eec9f375bcfa02
detbin <116663162+detbin@users.noreply.github.com>
2022-12-29 16:20:18 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ alumno= {
 sessionStorage.setItem('estudiante',alumno)
 localStorage.setItem('estudiante',alumno)
 document.cookie=("Nombre"+alumno)
-document.cookie="nombreCaducidad=Nombre;expires="+new Date(2023,14,08).toUTCString()
+document.cookie="nombreCaducidad=Nombre;expires="+new Date(120000).toUTCString()
